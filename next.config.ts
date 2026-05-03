7fecdcf782cecad4464936835f2f5e00dbbf464a
Tyler Sue <tylerhenrysue@gmail.com>
2026-05-02 21:44:46 -0700

    Fix image loading - unoptimized true

--- next.config.ts
+++ next.config.ts
@@ -8,12 +8,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
   },
 };
 
